a1fdae77fbf9f7b6272733d0169adb4c277dfaed
Daria Babakova <kofaka@gmail.com>
2018-10-29 12:58:21 +0200

    Refactor ThreeJsModel component to be a little bit more consist.

--- webgl-app/src/ThreeJsModel.js
+++ webgl-app/src/ThreeJsModel.js
@@ -17,53 +17,72 @@ class ThreeJsModel extends Component {
         this.renderer.setSize(width, height);
         this.renderer.setClearColor(0x7812d6, 1);
 
-        this.material = null;
-        this.mesh = {};
-        this.geometry = new THREE.SphereGeometry(24, 32, 32);
-        THREE.ImageUtils.crossOrigin = true;
-        this.textureLoader = new THREE.TextureLoader();
-        this.textureLoader.crossOrigin = true;
-
-        // so many lights
-        this.light1 = new THREE.DirectionalLight( 0xffffff, 1 );
-        this.light1.position.set( 0, 1, 0 );
-        this.scene.add( this.light1 );
-
-        this.light2 = new THREE.DirectionalLight( 0xffffff, 0.5 );
-        this.light2.position.set( 0, -1, 0 );
-        this.scene.add( this.light2 );
-
-        this.light3 = new THREE.DirectionalLight( 0xffffff, 1 );
-        this.light3.position.set( 1, 0, 0 );
-        this.scene.add( this.light3 );
-
-        this.light4 = new THREE.DirectionalLight( 0xffffff, 0.5 );
-        this.light4.position.set( 0, 0, 1 );
-        this.scene.add( this.light4 );
-
-        this.light5 = new THREE.DirectionalLight( 0xffffff, 1 );
-        this.light5.position.set( 0, 0, -1 );
-        this.scene.add( this.light5 );
-
-        this.light6 = new THREE.DirectionalLight( 0xffffff, 0.5 );
-        this.light6.position.set( -1, 0, 0 );
-        this.scene.add( this.light6 );
+        this.sphereToRotate = {};
 
         this.animate = this.animate.bind(this);
+        this.addManyLights = this.addManyLights.bind(this);
+        this.addTexturedSphere = this.addTexturedSphere.bind(this);
     }
 
     componentDidMount() {
         this.threeJsModel.appendChild(this.renderer.domElement);
+        this.addManyLights();
+        this.addTexturedSphere();
+    }
 
-        this.textureLoader.load(
+    addManyLights() {
+        const lightsList = [
+            {
+                directionalLights: [0xffffff, 1],
+                position: [0, 1, 0],
+            },
+            {
+                directionalLights: [0xffffff, 0.5],
+                position: [0, -1, 0],
+            },
+            {
+                directionalLights: [0xffffff, 1],
+                position: [1, 0, 0],
+            },
+            {
+                directionalLights: [0xffffff, 0.5],
+                position: [0, 0, 1],
+            },
+            {
+                directionalLights: [0xffffff, 1],
+                position: [0, 0, -1],
+            },
+            {
+                directionalLights: [0xffffff, 0.5],
+                position: [-1, 0, 0],
+            },
+        ];
+
+        lightsList.map(item => {
+            const light = new THREE.DirectionalLight(...item.directionalLights);
+            light.position.set(...item.position);
+            this.scene.add(light);
+        });
+    }
+
+    addTexturedSphere() {
+        // Add 'crossOrigin' tu turn on ability to use external link to to textures
+        THREE.ImageUtils.crossOrigin = true;
+        const textureLoader = new THREE.TextureLoader();
+        textureLoader.crossOrigin = true;
+
+        let material = null;
+        const geometry = new THREE.SphereGeometry(24, 32, 32);
+        const color = new THREE.Color('#009900');
+
+        textureLoader.load(
             './rank_3_police_unit/textures/ctrl_eye_baseColor.png',
             texture => {
                 texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
                 texture.repeat.set(2, 2);
-                const color = new THREE.Color('#009900');
-                this.material = new THREE.MeshPhongMaterial({color: color.getHex(), bumpMap: texture});
-                this.mesh = new THREE.Mesh(this.geometry, this.material);
-                this.scene.add(this.mesh);
+                material = new THREE.MeshPhongMaterial({color: color.getHex(), bumpMap: texture});
+                this.sphereToRotate = new THREE.Mesh(geometry, material);
+                this.scene.add(this.sphereToRotate);
                 this.camera.updateProjectionMatrix();
                 this.animate();
             },
@@ -74,8 +93,8 @@ class ThreeJsModel extends Component {
 
     animate() {
         requestAnimationFrame(this.animate);
-        this.mesh.rotation.x += 0.01;
-        this.mesh.rotation.y -= 0.01;
+        this.sphereToRotate.rotation.x += 0.01;
+        this.sphereToRotate.rotation.y -= 0.01;
 
         this.renderer.render(this.scene, this.camera);
     }
